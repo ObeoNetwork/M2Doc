f15d1d08e42dd2c66ba2c06501653e08dca12f1d
Romain Guider <romain.guider@obeo.fr>
2016-02-26 14:15:06 +0100

    [Test] extended template processing tests
     Completed the test coverage for if's and queries.

--- org.eclipse.gendoc2.test/src/org/eclipse/gendoc2/generator/test/TemplateProcessorTest.java
+++ org.eclipse.gendoc2.test/src/org/eclipse/gendoc2/generator/test/TemplateProcessorTest.java
@@ -37,6 +37,13 @@ public class TemplateProcessorTest {
 		return document;
 	}
 
+	/**
+	 * Test the replacement of a variable in a doc.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
 	@Test
 	public void testVarRefProcessing() throws InvalidFormatException, IOException, DocumentParserException {
 		FileInputStream is = new FileInputStream("templates/testVar.docx");
@@ -80,20 +87,20 @@ public class TemplateProcessorTest {
 
 	@Test
 	public void testQueryProcessing() throws InvalidFormatException, IOException, DocumentParserException {
-		FileInputStream is = new FileInputStream("templates/testVar.docx");
+		FileInputStream is = new FileInputStream("templates/testAQL.docx");
 		OPCPackage oPackage = OPCPackage.open(is);
 		XWPFDocument document = new XWPFDocument(oPackage);
 		BodyParser parser = new BodyParser(document, env);
 		Template template = parser.parseTemplate();
 		Map<String, Object> definitions = new HashMap<String, Object>();
-		definitions.put("x", "valueofx");
-		XWPFDocument destinationDoc = createDestinationDocument("templates/testVar.docx");
+		definitions.put("self", EcorePackage.eINSTANCE);
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testAQL.docx");
 		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
 		processor.doSwitch(template);
 		// scan the destination document
 		assertEquals(2, destinationDoc.getParagraphs().size());
 		System.out.println(destinationDoc.getParagraphs().get(0).getText());
-		assertEquals("Template de test pour les balises de référence à une variable\u00a0: valueofx",
+		assertEquals("Template de test pour les balises de query aql\u00a0: ecore",
 				destinationDoc.getParagraphs().get(0).getText());
 		assertEquals("Fin du gabarit", destinationDoc.getParagraphs().get(1).getText());
 	}
@@ -135,44 +142,197 @@ public class TemplateProcessorTest {
 				"Template de test pour les balises de répétition: name = EAttribute, name = EAnnotation, name = EClass, name = EClassifier, name = EDataType, name = EEnum, name = EEnumLiteral, name = EFactory, name = EModelElement, name = ENamedElement, name = EObject, name = EOperation, name = EPackage, name = EParameter, name = EReference, name = EStructuralFeature, name = ETypedElement, name = EStringToStringMapEntry, name = EGenericType, name = ETypeParameter, name = EBigDecimal, name = EBigInteger, name = EBoolean, name = EBooleanObject, name = EByte, name = EByteArray, name = EByteObject, name = EChar, name = ECharacterObject, name = EDate, name = EDiagnosticChain, name = EDouble, name = EDoubleObject, name = EEList, name = EEnumerator, name = EFeatureMap, name = EFeatureMapEntry, name = EFloat, name = EFloatObject, name = EInt, name = EIntegerObject, name = EJavaClass, name = EJavaObject, name = ELong, name = ELongObject, name = EMap, name = EResource, name = EResourceSet, name = EShort, name = EShortObject, name = EString, name = ETreeIterator, name = EInvocationTargetException, ",
 				destinationDoc.getParagraphs().get(0).getText());
 	}
-	//
-	// @Test
-	// public void testVarRefStyledMultipleParagraphsProcessing()
-	// throws InvalidFormatException, IOException, DocumentParserException {
-	// FileInputStream is = new
-	// FileInputStream("templates/testVarStyleSpanning2Paragraphs.docx");
-	// OPCPackage oPackage = OPCPackage.open(is);
-	// XWPFDocument document = new XWPFDocument(oPackage);
-	// DocumentParser parser = new DocumentParser(document, env);
-	// Template template = parser.parseTemplate();
-	// Map<String, Object> definitions = new HashMap<String, Object>();
-	// definitions.put("x", "valueofx");
-	// TemplateProcessor processor = new TemplateProcessor(definitions, env);
-	// AbstractConstruct result =
-	// processor.doSwitch(template.getSubConstructs().get(1));
-	// assertTrue(result instanceof StaticFragment);
-	// XWPFRun run = result.getRuns().get(0);
-	// assertEquals("valueofx", run.getText(run.getTextPosition()));
-	// assertEquals("E36C0A", run.getColor());
-	// assertNotNull(run.getCTR().getRPr().getI());
-	// assertNotNull(run.getCTR().getRPr().getB());
-	// }
-	//
-	// @Test
-	// public void testQueryProcessing() throws InvalidFormatException,
-	// IOException, DocumentParserException {
-	// FileInputStream is = new FileInputStream("templates/testAQL.docx");
-	// OPCPackage oPackage = OPCPackage.open(is);
-	// XWPFDocument document = new XWPFDocument(oPackage);
-	// DocumentParser parser = new DocumentParser(document, env);
-	// Template template = parser.parseTemplate();
-	// Map<String, Object> definitions = new HashMap<String, Object>();
-	// definitions.put("self", EcorePackage.eINSTANCE);
-	// TemplateProcessor processor = new TemplateProcessor(definitions, env);
-	// AbstractConstruct result =
-	// processor.doSwitch(template.getSubConstructs().get(1));
-	// assertTrue(result instanceof StaticFragment);
-	// XWPFRun run = result.getRuns().get(0);
-	// assertEquals("ecore", run.getText(run.getTextPosition()));
-	// }
+
+	/**
+	 * Tests a gd:if with <code>true</code> expression evaluation and without an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF1Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal1.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value1");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal1.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de value1",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>false</code> expression evaluation and without
+	 * an else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF2Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal1.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal1.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>true</code> expression evaluation and with an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF3Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal2.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value1");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal2.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de value1",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>false</code> expression evaluation and with an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF4Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal2.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal2.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de default value",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>false</code> expression evaluation and with an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF5Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal5.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value1");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal5.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de value1",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>false</code> expression evaluation and with an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF6Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal5.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value2");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal5.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de value2",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>false</code> expression evaluation and with an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF7Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal5.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal5.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de default value",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
+	/**
+	 * Tests a gd:if with <code>false</code> expression evaluation and with an
+	 * else.
+	 * 
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 * @throws DocumentParserException
+	 */
+	@Test
+	public void testGDIF8Processing() throws InvalidFormatException, IOException, DocumentParserException {
+		FileInputStream is = new FileInputStream("templates/testConditionnal6.docx");
+		OPCPackage oPackage = OPCPackage.open(is);
+		XWPFDocument document = new XWPFDocument(oPackage);
+		BodyParser parser = new BodyParser(document, env);
+		Template template = parser.parseTemplate();
+		Map<String, Object> definitions = new HashMap<String, Object>();
+		definitions.put("x", "value1");
+		XWPFDocument destinationDoc = createDestinationDocument("templates/testConditionnal6.docx");
+		TemplateProcessor processor = new TemplateProcessor(definitions, env, destinationDoc);
+		processor.doSwitch(template);
+		assertEquals("Template de test pour les balises conditionnelles\u00a0: ajout de value1",
+				destinationDoc.getParagraphs().get(0).getText());
+	}
+
 }
