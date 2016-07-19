701f254a3aa5f44d98007c711cfbe2b3b57d24d1
Nathalie Lepine <nathalie.lepine@obeo.fr>
2016-07-19 12:06:01 +0200

    manage genconf icons
    Change-Id: Ib58417acc9b360c4a512efd0fe475e8e007f6dff

--- plugins/org.obeonetwork.m2doc.genconf.edit/src-gen/org/obeonetwork/m2doc/genconf/provider/DefinitionItemProvider.java
+++ plugins/org.obeonetwork.m2doc.genconf.edit/src-gen/org/obeonetwork/m2doc/genconf/provider/DefinitionItemProvider.java
@@ -88,6 +88,16 @@ public class DefinitionItemProvider
     }
 
 	/**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected boolean shouldComposeCreationImage() {
+        return true;
+    }
+
+    /**
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
