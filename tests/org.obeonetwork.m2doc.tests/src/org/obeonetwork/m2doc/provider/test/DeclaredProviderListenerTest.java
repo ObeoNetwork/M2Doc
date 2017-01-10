c9436a6bb7a64785293fbe496f7eb3614bf2002b
Yvan Lussaud <yvan.lussaud@obeo.fr>
2017-01-10 14:29:01 +0100

    Fixed DeclaredProviderListenerTest.

--- tests/org.obeonetwork.m2doc.tests/src/org/obeonetwork/m2doc/provider/test/DeclaredProviderListenerTest.java
+++ tests/org.obeonetwork.m2doc.tests/src/org/obeonetwork/m2doc/provider/test/DeclaredProviderListenerTest.java
@@ -49,7 +49,7 @@ public class DeclaredProviderListenerTest {
         assertTrue(findErrorWithMessage(errorLogListener.getAllStatus(),
                 "Problem while registering M2Doc Providers : Plug-in org.obeonetwork.m2doc.tests was unable to load class org.obeonetwork.m2doc.test.Wrong."));
         assertTrue(findErrorWithMessage(errorLogListener.getAllStatus(),
-                "Problem while registering M2Doc Providers : the provider \"org.obeonetwork.m2doc.provider.test.StubDiagramProvider\" is already registered. The current implementation will not be used."));
+                "Problem while registering M2Doc Providers : the provider \"org.obeonetwork.m2doc.provider.test.TestDiagramProvider\" is already registered. The current implementation will not be used."));
     }
 
     /**
