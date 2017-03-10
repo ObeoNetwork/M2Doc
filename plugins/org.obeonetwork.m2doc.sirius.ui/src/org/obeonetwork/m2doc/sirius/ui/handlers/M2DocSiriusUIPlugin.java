c97ad0b531bb01456c8e19c45f01dc9f9a1f9d6c
cbrun <cedric.brun@obeo.fr>
2017-03-10 10:21:27 +0100

    Fix #126 by moving the action code to a specific plugin
    As a sidenote It seems the code for this action had a number of issues,
    I kept it "as-is" as I'm not sure which scenario it was supposed to fix
    and if this got tested but:
    - the ./icons folder was not exported as part of the build.properties
    - the plugin.xml contribute a command called
    "org.obeonetwork.m2doc.sirius.commands.sampleCommand" which doesn't seem
    defined anywhere.
    - tooltip="Say hello world" is probably not relevant.

---
+++ plugins/org.obeonetwork.m2doc.sirius.ui/src/org/obeonetwork/m2doc/sirius/ui/handlers/M2DocSiriusUIPlugin.java
@@ -0,0 +1,77 @@
+package org.obeonetwork.m2doc.sirius.ui.handlers;
+/*******************************************************************************
+
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.ResourceLocator;
+
+/**
+ * Plugin and Activator classes for the bundle.
+ * 
+ * @author <a href="mailto:cedric.brun@obeo.fr">Cedric Brun</a>
+ */
+public final class M2DocSiriusUIPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 */
+	public static final M2DocSiriusUIPlugin INSTANCE = new M2DocSiriusUIPlugin();
+
+	public static final String PLUGIN_ID = "org.eclipse.m2doc.sirius.ui";
+
+	/**
+	 * Keep track of the singleton.
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 */
+	public M2DocSiriusUIPlugin() {
+		super(new ResourceLocator[] {});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * 
+	 * @return the singleton instance.
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * 
+	 * @return the singleton instance.
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 */
+	public static class Implementation extends EclipsePlugin {
+		/**
+		 * Creates an instance.
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
