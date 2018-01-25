77d9383105273ed4759b949481c1919e0b2ccf46
Yvan Lussaud <yvan.lussaud@obeo.fr>
2018-01-25 09:45:06 +0100

    Added echo to promote.sh.

--- promote.sh
+++ promote.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 # add document page to menu
+echo "Create entry $1 in the ref-doc menu."
 sed -i "s+#---TAG---+#---TAG---\n  - title: $1\n    file: ref-doc/$1/index+g" _data/pages.yml
 
 # reference the current version in the toc
+echo "Change nightly to $1 in ref-doc/index-toc.xml."
 sed -i "s+nightly+$1+g" ref-doc/index-toc.xml
 
 # initialize documentation
+echo "Copy documentation to ref-doc/$1."
 cp -Rf ref-doc/nightly/ ref-doc/$1
 
 #update the documentation
+echo "Make other replacements in ref-doc/$1/index.md."
 sed -i "s+article-with-toc-nightly+article-with-toc+g" ref-doc/$1/index.md
 sed -i "s+nightly+$1+g" ref-doc/$1/index.md
 sed -i "s+Nightly+$1+g" ref-doc/$1/index.md
 sed -i "s+master+$1+g" ref-doc/$1/index.md
 
 #commit
+echo "Commit ref-doc/$1."
 git config user.email "yvan.lussaud@obeo.fr"
 git config user.name "Yvan Lussaud"
 git add _data/pages.yml
