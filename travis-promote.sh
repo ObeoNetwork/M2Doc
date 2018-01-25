eda2432128aa77b76f742311f291110691b9b3de
Yvan Lussaud <yvan.lussaud@obeo.fr>
2018-01-25 09:12:35 +0100

    Echo promote status in travis-promote.sh.

--- travis-promote.sh
+++ travis-promote.sh
@@ -5,5 +5,6 @@ echo "Build $TRAVIS_JOB_NUMBER"
 echo "Git: $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
 echo "Root dir: $TRAVIS_BUILD_DIR"
 cd $TRAVIS_BUILD_DIR
+echo "Promote: $UPDATE_SITES"
 echo "Build promoted."
 fi
