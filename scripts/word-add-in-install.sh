69c4707510833ca7878fece11f8f4b84bc429dd9
Yvan Lussaud <yvan.lussaud@obeo.fr>
2020-01-27 14:35:29 +0100

    Fixed word-add-in-install.sh.

--- scripts/word-add-in-install.sh
+++ scripts/word-add-in-install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo add-apt-repository ppa:ubuntu-mozilla-security/ppa 
-sudo apt-get update
+sudo add-apt-repository -y -u ppa:ubuntu-mozilla-security/ppa 
 sudo apt-get install firefox-geckodriver
 cd add-in
 npm install
