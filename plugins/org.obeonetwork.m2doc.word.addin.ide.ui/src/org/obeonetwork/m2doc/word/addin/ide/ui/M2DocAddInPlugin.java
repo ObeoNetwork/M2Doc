1be73de7d16e04debbcc77aabe71549ed6c9071e
Yvan Lussaud <yvan.lussaud@obeo.fr>
2020-07-03 15:06:26 +0200

    Added export package to org.obeonetwork.m2doc.word.addin.ide.ui.

--- plugins/org.obeonetwork.m2doc.word.addin.ide.ui/src/org/obeonetwork/m2doc/word/addin/ide/ui/M2DocAddInPlugin.java
+++ plugins/org.obeonetwork.m2doc.word.addin.ide.ui/src/org/obeonetwork/m2doc/word/addin/ide/ui/M2DocAddInPlugin.java
@@ -1,7 +1,5 @@
-package org.obeonetwork.m2doc.word.addin.ide.ui;
 /*******************************************************************************
-
- * Copyright (c) 2016 Obeo.
+ * Copyright (c) 2019 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +8,7 @@ package org.obeonetwork.m2doc.word.addin.ide.ui;
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
+package org.obeonetwork.m2doc.word.addin.ide.ui;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
