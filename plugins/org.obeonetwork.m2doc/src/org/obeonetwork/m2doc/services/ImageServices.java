049398d63ae274170131bf460b0ebedca510206e
Arthur Daussy <arthur.daussy@obeo.fr>
2020-11-26 17:58:43 +0100

    Fixed #392 Improve URI encoding to handle with spaces in path

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/services/ImageServices.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/services/ImageServices.java
@@ -72,7 +72,7 @@ public class ImageServices {
     )
     // @formatter:on
     public MImage asImage(String uriStr) {
-        final URI imageURI = URI.createURI(uriStr, false);
+        final URI imageURI = URI.createURI(uriStr, true);
         return asImage(uriStr, PictureType.toType(imageURI));
     }
 
@@ -102,7 +102,7 @@ public class ImageServices {
      * @return the {@link MImage} corresponding to the given path
      */
     private MImage asImage(String uriStr, PictureType type) {
-        final URI imageURI = URI.createURI(uriStr, false);
+        final URI imageURI = URI.createURI(uriStr, true);
         final URI uri = imageURI.resolve(templateURI);
 
         return new MImageImpl(uriConverter, uri, type);
