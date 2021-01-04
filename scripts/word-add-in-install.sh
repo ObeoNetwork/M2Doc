923ad7efe3b2a46d086388955bef78d898e8fe4d
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-01-04 14:52:11 +0100

    Moving from geckodriver 0.28.0 to 0.20.1.

--- scripts/word-add-in-install.sh
+++ scripts/word-add-in-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://github.com/mozilla/geckodriver/releases/download/v0.28.0/geckodriver-v0.28.0-linux64.tar.gz
-tar xfz geckodriver-v0.28.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz
+tar xfz geckodriver-v0.20.1-linux64.tar.gz
 sudo mv geckodriver /usr/local/bin/
 sudo chmod +x /usr/local/bin/geckodriver
 
