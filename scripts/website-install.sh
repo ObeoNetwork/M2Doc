2b47a29d4c4396053afe71b0f0ecf4abc198067c
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-01-08 17:13:28 +0100

    Added documentation and add-in build.

--- scripts/website-install.sh
+++ scripts/website-install.sh
@@ -2,11 +2,11 @@
 sudo add-apt-repository ppa:brightbox/ruby-ng -y
 sudo apt-get update
 sudo apt-get install ruby-dev ruby-full
-gem install addressable --version '= 2.4.0'
-gem install jekyll -v 3.8.6
-gem install jekyll-paginate
-gem install jekyll-gist
-gem install pygments.rb
-gem install redcarpet
-gem install RedCloth
+sudo gem install addressable --version '= 2.4.0'
+sudo gem install jekyll -v 3.8.6
+sudo gem install jekyll-paginate
+sudo gem install jekyll-gist
+sudo gem install pygments.rb
+sudo gem install redcarpet
+sudo gem install RedCloth
 
