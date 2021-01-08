0a0148434d2df4143736047bf8ed9ffbe4124405
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-01-08 15:43:57 +0100

    Added documentation and add-in build.

--- scripts/website-build.sh
+++ scripts/website-build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+TAG=$1
+
 mkdir web-site
 cd web-site
 echo "Clone gh-pages"
 git clone https://$GH_TOKEN@github.com/ObeoNetwork/M2Doc.git -b gh-pages --quiet .
-if [[ "$TRAVIS_TAG" != "" ]]; then chmod a+x ./promote.sh; ./promote.sh ${TRAVIS_TAG}; fi
+if [[ "$TAG" != "" ]]; then chmod a+x ./promote.sh; ./promote.sh ${TAG}; fi
 echo "Build gh-pages"
 jekyll build
 ls
