dcb565ddfed2dcaa5f4d5dba49f6840a4e855931
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-01-11 09:51:16 +0100

    Make PROFILE global.

--- scripts/website-build.sh
+++ scripts/website-build.sh
@@ -5,7 +5,7 @@ mkdir web-site
 cd web-site
 echo "Clone gh-pages"
 git clone https://$GH_TOKEN@github.com/ObeoNetwork/M2Doc.git -b gh-pages --quiet .
-if [[ "$TAG" != "" ]]; then chmod a+x ./promote.sh; ./promote.sh ${TAG}; fi
+if [[ "$TAG" != "" ]]; then chmod a+x ./promote.sh; ./promote.sh $TAG; fi
 echo "Build gh-pages"
 jekyll build
 ls
