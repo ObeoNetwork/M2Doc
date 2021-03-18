3db44674a65f9eed6fcadda3aef71be3a77a484a
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-03-18 15:04:38 +0100

    Fixed #417 Add support for XWPF* elements in HtmlSerializer.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/HtmlSerializer.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/HtmlSerializer.java
@@ -18,7 +18,21 @@ import java.io.InputStream;
 import java.util.Base64;
 import java.util.Collection;
 
+import org.apache.poi.openxml4j.opc.PackageRelationship;
 import org.apache.poi.util.IOUtils;
+import org.apache.poi.xwpf.usermodel.IBody;
+import org.apache.poi.xwpf.usermodel.IBodyElement;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
+import org.apache.poi.xwpf.usermodel.XWPFHyperlinkRun;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFPicture;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFStyle;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell.XWPFVertAlign;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.obeonetwork.m2doc.element.MBookmark;
 import org.obeonetwork.m2doc.element.MElement;
 import org.obeonetwork.m2doc.element.MHyperLink;
@@ -31,6 +45,9 @@ import org.obeonetwork.m2doc.element.MTable;
 import org.obeonetwork.m2doc.element.MTable.MCell;
 import org.obeonetwork.m2doc.element.MTable.MRow;
 import org.obeonetwork.m2doc.element.MText;
+import org.obeonetwork.m2doc.generator.GenerationResult;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTStyle;
 
 /**
  * HTML serializer.
@@ -61,6 +78,10 @@ public class HtmlSerializer {
 
         if (object instanceof MElement) {
             res = serialize((MElement) object);
+        } else if (object instanceof GenerationResult) {
+            res = serialize((GenerationResult) object);
+        } else if (object instanceof IBody) {
+            res = serialize((IBody) object);
         } else if (object instanceof Collection) {
             final StringBuilder builder = new StringBuilder();
             for (Object child : (Collection<?>) object) {
@@ -271,6 +292,294 @@ public class HtmlSerializer {
             if (!spawnStyle.isEmpty()) {
                 res = "<spawn style=\"" + spawnStyle + "\">" + res + "</spawn>";
             }
+        }
+
+        return res;
+    }
+
+    /**
+     * Serialize the given {@link GenerationResult} into HTML.
+     * 
+     * @param result
+     *            the {@link GenerationResult}
+     * @return the given {@link GenerationResult} into HTML
+     */
+    private String serialize(GenerationResult result) {
+        // TODO messages ?
+        return serialize(result.getBody());
+    }
+
+    /**
+     * Serialize the given {@link IBody} into HTML.
+     * 
+     * @param body
+     *            the {@link IBody}
+     * @return the given {@link IBody} into HTML
+     */
+    public String serialize(IBody body) {
+        final StringBuilder res = new StringBuilder();
+
+        for (IBodyElement element : body.getBodyElements()) {
+            res.append(serialize(element));
+        }
+
+        return res.toString();
+    }
+
+    /**
+     * Serialize the given {@link IBodyElement} into HTML.
+     * 
+     * @param element
+     *            the {@link IBodyElement}
+     * @return the given {@link IBodyElement} into HTML
+     */
+    public String serialize(IBodyElement element) {
+        final String res;
+
+        if (element instanceof XWPFParagraph) {
+            res = serialize((XWPFParagraph) element);
+        } else if (element instanceof XWPFTable) {
+            res = serialize((XWPFTable) element);
+        } else {
+            res = "";
+        }
+
+        return res;
+    }
+
+    /**
+     * Serialize the given {@link XWPFParagraph} into HTML.
+     * 
+     * @param paragraph
+     *            the {@link XWPFParagraph}
+     * @return the given {@link XWPFParagraph} into HTML
+     */
+    private String serialize(XWPFParagraph paragraph) {
+        final StringBuilder res = new StringBuilder();
+
+        // TODO numbering
+        String paragraphStyle = "";
+        final XWPFParagraph localParagraph = new XWPFParagraph(paragraph.getCTP(), paragraph.getBody());
+        if (localParagraph.getStyleID() != null) {
+            final XWPFStyle style = localParagraph.getBody().getXWPFDocument().getStyles()
+                    .getStyle(localParagraph.getStyleID());
+            final CTStyle ctStyle = style.getCTStyle();
+            if (ctStyle.isSetRPr()) {
+                final CTRPr rPr = ctStyle.getRPr();
+                if (rPr.isSetSz()) {
+                    double size = rPr.getSz().getVal().doubleValue();
+                    paragraphStyle += String.format("font-size:%spx;", size);
+                }
+                if (rPr.isSetColor()) {
+                    final String color = rPr.getColor().xgetVal().getStringValue();
+                    paragraphStyle += String.format("color:#%s;", color);
+                }
+            }
+        }
+        if (localParagraph.getAlignment() != null) {
+            paragraphStyle += String.format("text-align:#%s;", getHTMLAlignment(localParagraph.getAlignment()));
+        }
+        if (!paragraphStyle.isEmpty()) {
+            res.append("<p style=\"" + paragraphStyle + "\">");
+        } else {
+            res.append("<p>");
+        }
+        for (XWPFRun run : localParagraph.getRuns()) {
+            final String escapedText = escapeHTML(run.getText(0));
+            final String formatedText = String.format(getStyleFormat(run), escapedText);
+            for (XWPFPicture picture : run.getEmbeddedPictures()) {
+                res.append(
+                        "<img src=\"data:" + picture.getPictureData().getPackagePart().getContentType() + ";base64, ");
+                res.append(new String(Base64.getEncoder().encode(picture.getPictureData().getData())));
+                res.append("\"/>");
+            }
+            if (run instanceof XWPFHyperlinkRun) {
+                final String id = ((XWPFHyperlinkRun) run).getCTHyperlink().getId();
+                final PackageRelationship relation = localParagraph.getBody().getPart().getPackagePart()
+                        .getRelationship(id);
+                final String url = relation.getSourceURI().toString();
+                res.append("<a href=\"" + escapeHTML(url) + "\">" + formatedText + "</a>");
+            } else {
+                res.append(formatedText);
+            }
+        }
+        res.append("</p>");
+
+        return res.toString();
+    }
+
+    /**
+     * Gets the string format for the given {@link XWPFRun}.
+     * 
+     * @param run
+     *            the {@link XWPFRun}
+     * @return the string format for the given {@link XWPFRun}
+     */
+    private String getStyleFormat(XWPFRun run) {
+        String res = "%s";
+
+        if (run.isBold()) {
+            res = "<strong>" + res + "</strong>";
+        }
+        if (run.isItalic()) {
+            res = "<i>" + res + "</i>";
+        }
+        if (run.isStrikeThrough()) {
+            res = "<del>" + res + "</del>";
+        }
+        if (run.getUnderline() != null && run.getUnderline() != UnderlinePatterns.NONE) {
+            res = "<u>" + res + "</u>";
+        }
+
+        String spawnStyle = "";
+        if (run.getFontSize() != -1) {
+            spawnStyle += String.format("font-size:%spx;", run.getFontSize());
+        }
+        if (run.getCTR().getRPr() != null && run.getCTR().getRPr().getShd() != null
+            && run.getCTR().getRPr().getShd().getFill() != null) {
+            // TODO double check this.
+            spawnStyle += String.format("background-color:#%s;", run.getCTR().getRPr().getShd().getFill());
+        }
+        if (run.getColor() != null) {
+            // TODO double check this.
+            spawnStyle += String.format("color:#%s;", run.getColor());
+        }
+        if (!spawnStyle.isEmpty()) {
+            res = "<spawn style=\"" + spawnStyle + "\">" + res + "</spawn>";
+        }
+
+        return res;
+    }
+
+    /**
+     * Gets an escaped HTML version of the given {@link String}.
+     * 
+     * @param str
+     *            the {@link String} to escape
+     * @return the escaped HTML version of the given {@link String}
+     */
+    public static String escapeHTML(String str) {
+        final StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            final char c = str.charAt(i);
+            switch (c) {
+                case '"':
+                case '\'':
+                case '<':
+                case '>':
+                case '&':
+                    res.append("&#");
+                    res.append((int) c);
+                    res.append(';');
+                    break;
+
+                default:
+                    res.append(c);
+                    break;
+            }
+        }
+
+        return res.toString();
+    }
+
+    /**
+     * Gets the HTML alignment for the given {@link ParagraphAlignment}.
+     * 
+     * @param alignment
+     *            the {@link ParagraphAlignment}
+     * @return the HTML alignment for the given {@link ParagraphAlignment}
+     */
+    private String getHTMLAlignment(ParagraphAlignment alignment) {
+        final String res;
+
+        switch (alignment) {
+            case BOTH:
+                res = "justify";
+                break;
+
+            case CENTER:
+                res = "center";
+                break;
+
+            case DISTRIBUTE:
+                res = "justify";
+                break;
+
+            case LEFT:
+                res = "left";
+                break;
+
+            case RIGHT:
+                res = "right";
+                break;
+
+            default:
+                res = "left";
+                break;
+        }
+
+        return res;
+    }
+
+    /**
+     * Serialize the given {@link XWPFTable} into HTML.
+     * 
+     * @param table
+     *            the {@link XWPFTable}
+     * @return the given {@link XWPFTable} into HTML
+     */
+    private String serialize(XWPFTable table) {
+        final StringBuilder res = new StringBuilder();
+
+        res.append("<table style=\"border: 1px solid black;border-collapse: collapse;\">");
+        final XWPFTable localTable = new XWPFTable(table.getCTTbl(), table.getBody());
+        for (XWPFTableRow row : localTable.getRows()) {
+            res.append("<trstyle=\"border: 1px solid black;\">");
+
+            for (XWPFTableCell cell : row.getTableCells()) {
+                // TODO merge cell
+                if (cell.getVerticalAlignment() != null) {
+                    res.append("<td style=\"border: 1px solid black;vertical-align:"
+                        + getHTMLAlignment(cell.getVerticalAlignment()) + ";\">");
+                } else {
+                    res.append("<td style=\"border: 1px solid black;\">");
+                }
+                res.append(serialize(cell));
+                res.append("</td>");
+            }
+            res.append("</tr>");
+
+        }
+        res.append("</table>");
+
+        return res.toString();
+    }
+
+    /**
+     * Gets the HTML alignement for the given {@link XWPFVertAlign}.
+     * 
+     * @param verticalAlignment
+     *            the {@link XWPFVertAlign}
+     * @return the HTML alignement for the given {@link XWPFVertAlign}
+     */
+    private String getHTMLAlignment(XWPFVertAlign verticalAlignment) {
+        final String res;
+
+        switch (verticalAlignment) {
+            case BOTTOM:
+                res = "bottom";
+                break;
+
+            case TOP:
+                res = "top";
+                break;
+
+            case CENTER:
+            case BOTH:
+            default:
+                res = "baseline";
+                break;
         }
 
         return res;
