b18cc422cc5e732a1a8043c5b8b711611c0cddc5
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-03-30 15:26:02 +0200

    Moving from 3.1.1 to 3.2.0.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2016 Obeo. 
+ *  Copyright (c) 2016, 2021 Obeo. 
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
@@ -89,7 +89,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "3.1.1";
+    public static final String VERSION = "3.2.0";
 
     /**
      * The plugin ID.
