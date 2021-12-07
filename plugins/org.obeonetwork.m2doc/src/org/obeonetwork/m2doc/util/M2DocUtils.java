daadcd6f51217a9f370c1e269b84932a8afe753e
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-12-07 14:13:21 +0100

    Moving from 3.2.0 to 3.2.1.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -89,7 +89,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "3.2.0";
+    public static final String VERSION = "3.2.1";
 
     /**
      * The plugin ID.
