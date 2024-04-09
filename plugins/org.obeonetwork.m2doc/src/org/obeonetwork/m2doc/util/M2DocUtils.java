9e2f9a9db3e539df29c8fc8b99fec774271289ac
Yvan Lussaud <yvan.lussaud@obeo.fr>
2024-04-09 11:02:11 +0200

    Moving from 3.3.2 to 3.3.3.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2016, 2023 Obeo. 
+ *  Copyright (c) 2016, 2024 Obeo. 
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
@@ -91,7 +91,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "3.3.2";
+    public static final String VERSION = "3.3.3";
 
     /**
      * The plugin ID.
