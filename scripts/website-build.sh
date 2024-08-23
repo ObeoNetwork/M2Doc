cea1578e8a194c615ae5b618fc892a6cf83a6e06
Yvan Lussaud <yvan.lussaud@obeo.fr>
2024-08-23 11:53:36 +0200

    Fixed #524 Online help missing.

--- scripts/website-build.sh
+++ scripts/website-build.sh
@@ -7,11 +7,28 @@ echo "Clone gh-pages"
 git clone https://$GH_TOKEN@github.com/ObeoNetwork/M2Doc.git -b gh-pages --quiet .
 if [[ "$TAG" != "" ]]; then chmod a+x ./promote.sh; ./promote.sh $TAG; fi
 echo "Build gh-pages"
-jekyll build
-ls
-# copy to documentation plugin
-echo "Copy to documentation plugin"
+# create documentation folder
 mkdir ../plugins/org.obeonetwork.m2doc.doc/doc
-cp -Rf _site/* ../plugins/org.obeonetwork.m2doc.doc/doc/
+# download the nighlty documentation
+echo " - Download nightly documentation"
+wget -r --no-parent --no-host-directories -P ../plugins/org.obeonetwork.m2doc.doc/doc/ https://www.m2doc.org/ref-doc/nightly/
+# replace nigthly by current tag
+if [ -n "$TAG" ]; then
+    echo " - Replace Nightly to" $TAG
+    find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i 's#Nightly#'$TAG'#g' {} \;
+    find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i 's#nightly#'$TAG'#g' {} \;
+fi
+# remove the navigation bar
+echo " - Remove navigation bar"
+find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i '/<nav/,/nav>/d' {} \;
+# replace all download versions link
+echo " - Replace all versions download link"
+find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i 's#../../download/#all-versions#https://github.com/ObeoNetwork/M2Doc/releases#g' {} \;
+# copy static resources
+echo " - Copy static resources"
+cp -R css ../plugins/org.obeonetwork.m2doc.doc/doc/
+cp -R images ../plugins/org.obeonetwork.m2doc.doc/doc/
+cp -R capella ../plugins/org.obeonetwork.m2doc.doc/doc/
+cp -R font-awesome ../plugins/org.obeonetwork.m2doc.doc/doc/
 cd -
 
