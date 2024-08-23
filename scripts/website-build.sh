f477d92e6485328269709933c86d3684ed70798b
Yvan Lussaud <yvan.lussaud@obeo.fr>
2024-08-23 12:01:31 +0200

    Make wget less verbose.

--- scripts/website-build.sh
+++ scripts/website-build.sh
@@ -5,13 +5,12 @@ mkdir web-site
 cd web-site
 echo "Clone gh-pages"
 git clone https://$GH_TOKEN@github.com/ObeoNetwork/M2Doc.git -b gh-pages --quiet .
-if [[ "$TAG" != "" ]]; then chmod a+x ./promote.sh; ./promote.sh $TAG; fi
 echo "Build gh-pages"
 # create documentation folder
 mkdir ../plugins/org.obeonetwork.m2doc.doc/doc
 # download the nighlty documentation
 echo " - Download nightly documentation"
-wget -r --no-parent --no-host-directories -P ../plugins/org.obeonetwork.m2doc.doc/doc/ https://www.m2doc.org/ref-doc/nightly/
+wget -r --no-parent --no-host-directories --no-verbose -P ../plugins/org.obeonetwork.m2doc.doc/doc/ https://www.m2doc.org/ref-doc/nightly/
 # replace nigthly by current tag
 if [ -n "$TAG" ]; then
     echo " - Replace Nightly to" $TAG
@@ -23,7 +22,7 @@ echo " - Remove navigation bar"
 find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i '/<nav/,/nav>/d' {} \;
 # replace all download versions link
 echo " - Replace all versions download link"
-find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i 's#../../download/#all-versions#https://github.com/ObeoNetwork/M2Doc/releases#g' {} \;
+find ../plugins/org.obeonetwork.m2doc.doc/doc/ -name "*.html" -exec sed -i 's_../../download/#all-versions_https://github.com/ObeoNetwork/M2Doc/releases_g' {} \;
 # copy static resources
 echo " - Copy static resources"
 cp -R css ../plugins/org.obeonetwork.m2doc.doc/doc/
