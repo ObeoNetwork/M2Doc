9da3127652a991b32581224ba756bf177e9b6026
Yvan Lussaud <yvan.lussaud@obeo.fr>
2024-09-02 14:35:01 +0200

    Updated Sirius services documentation.

--- plugins/org.obeonetwork.m2doc.sirius/src/org/obeonetwork/m2doc/sirius/services/M2DocSiriusServices.java
+++ plugins/org.obeonetwork.m2doc.sirius/src/org/obeonetwork/m2doc/sirius/services/M2DocSiriusServices.java
@@ -63,7 +63,7 @@ import org.obeonetwork.m2doc.sirius.util.DTable2MTableConverter;
 
 //@formatter:off
 @ServiceProvider(
-value = "Services available for Sirius. You will have to set the \"SiriusSession\" option in the generation configuration. It must contains the .aird file URI. See [document examples](https://github.com/ObeoNetwork/M2Doc/tree/master/tests/org.obeonetwork.m2doc.sirius.tests/resources/m2DocSiriusServices). In addition, you can force the refresh of all representation with option \"SiriusForceRefresh\" set to \"true\". Note this option might have an impact on memory usage, calls to services with the refresh parameter should be prefered."
+value = "Services available for Sirius. You will have to set the \"SiriusSession\" option in the generation configuration. It must contains the .aird file URI. See [document examples](https://github.com/ObeoNetwork/M2Doc/tree/master/tests/org.obeonetwork.m2doc.sirius.tests/resources/m2DocSiriusServices). In addition, you can force the refresh of all representation with option \"SiriusForceRefresh\" set to \"true\". Note this option might have an impact on memory usage, calls to services with the refresh parameter should be prefered. You can also use the two following options to define the size of the exported images from Sirius diagrams: \"SiriusScalingPolicy\" that can be one of: WORKSPACE_DEFAULT, AUTO_SCALING, NO_SCALING, AUTO_SCALING_IF_LARGER and \"SiriusScaleLevel\" and integer."
 )
 //@formatter:on
 @SuppressWarnings({"checkstyle:javadocmethod", "checkstyle:javadoctype"})
