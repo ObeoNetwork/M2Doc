fda1a399fa8a5384a999ed24f332d6436fb520ac
Yvan Lussaud <yvan.lussaud@obeo.fr>
2024-12-03 14:34:55 +0100

    Moving from 3.3.3 to 3.3.4.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -91,7 +91,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "3.3.3";
+    public static final String VERSION = "3.3.4";
 
     /**
      * The plugin ID.
--- plugins/org.obeonetwork.m2doc/utils/org/obeonetwork/m2doc/TemplateVersionMigration.java
+++ plugins/org.obeonetwork.m2doc/utils/org/obeonetwork/m2doc/TemplateVersionMigration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2022 Obeo. 
+ *  Copyright (c) 2022, 2024 Obeo. 
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
@@ -66,7 +66,7 @@ public final class TemplateVersionMigration {
                             uri)) {
                         TemplateCustomProperties properties = new TemplateCustomProperties(xwpfDocument);
                         // TODO update version number
-                        if ("3.2.2".equals(properties.getM2DocVersion())) {
+                        if ("3.3.3".equals(properties.getM2DocVersion())) {
                             properties.setM2DocVersion(M2DocUtils.VERSION);
                             properties.save();
                             POIServices.getInstance().saveFile(URIConverter.INSTANCE, xwpfDocument, uri);
