dc8fb2eba4ee6f26f0371d8cc635acf9aa0dcee1
Yvan Lussaud <yvan.lussaud@obeo.fr>
2025-09-23 16:59:28 +0200

    Moved from M2Doc 4.0.0 to 5.0.0.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -102,7 +102,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "4.0.0";
+    public static final String VERSION = "5.0.0";
 
     /**
      * The plugin ID.
