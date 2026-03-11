e5cec6bd5f541d1fc1a659af8ead7e67284473b0
Yvan Lussaud <yvan.lussaud@obeo.fr>
2026-03-11 10:38:52 +0100

    Fixed #554 Fix support of internal links for generation from html data.

--- plugins/org.obeonetwork.m2doc.html/src/org/obeonetwork/m2doc/html/services/M2DocHTMLParser.java
+++ plugins/org.obeonetwork.m2doc.html/src/org/obeonetwork/m2doc/html/services/M2DocHTMLParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2019, 2025 Obeo. 
+ *  Copyright (c) 2019, 2026 Obeo. 
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
@@ -1874,7 +1874,12 @@ public class M2DocHTMLParser extends Parser {
         if (file.isAbsolute()) {
             res = URI.createFileURI(uri);
         } else {
-            res = URI.createURI(uri.replace('\\', '/')).resolve(baseURI);
+            final URI targetURI = URI.createURI(uri.replace('\\', '/'));
+            if (targetURI.isCurrentDocumentReference()) {
+                res = targetURI;
+            } else {
+                res = targetURI.resolve(baseURI);
+            }
         }
 
         return res;
