9c5924b588c253c01ad91be2f6291febdc2f2054
Yvan Lussaud <yvan.lussaud@obeo.fr>
2026-05-21 14:53:14 +0200

    Moving from 4.0.1 to 4.0.2.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2016, 2025 Obeo. 
+ *  Copyright (c) 2016, 2026 Obeo. 
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
@@ -94,7 +94,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "4.0.1";
+    public static final String VERSION = "4.0.2";
 
     /**
      * The plugin ID.
