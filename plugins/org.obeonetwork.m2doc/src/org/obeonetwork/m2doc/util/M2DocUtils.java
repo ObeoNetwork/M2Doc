af888269c1af8f26427193648bfa423cbcbe5f48
Yvan Lussaud <yvan.lussaud@obeo.fr>
2026-05-21 13:04:02 +0200

    Moving from 3.3.5 to 3.3.6.

--- plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
+++ plugins/org.obeonetwork.m2doc/src/org/obeonetwork/m2doc/util/M2DocUtils.java
@@ -91,7 +91,7 @@ public final class M2DocUtils {
     /**
      * M2Doc version.
      */
-    public static final String VERSION = "3.3.5";
+    public static final String VERSION = "3.3.6";
 
     /**
      * The plugin ID.
